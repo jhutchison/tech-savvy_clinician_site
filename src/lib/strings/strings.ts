5f8a49f93679cee30f70d6f7a65833e73c7cba04
John Hutchison <jhutchison@gmail.com>
2026-04-16 13:46:48 -0400

    add email contact button

--- src/lib/strings/strings.ts
+++ src/lib/strings/strings.ts
@@ -8,8 +8,8 @@ export const aboutUsText = "Our company was founded by experienced clinicians an
 
 export const companyName = "The Tech-Savvy Clinician";
 
-export const intakeEmail = "intake@techsavvyclinician.com";
+export const initialContactEmail = "intake@techsavvyclinician.com";
 
-const intakeMailSubject = "Website inquiry";
-export const intakeMailtoHref = `mailto:${intakeEmail}?subject=${encodeURIComponent(intakeMailSubject)}`;
+const initialContactEmailSubject = "Tech-Savvy Clinician Inital Inquiry";
+export const intakeMailtoHref = `mailto:${initialContactEmail}?subject=${encodeURIComponent(initialContactEmailSubject)}`;
 
